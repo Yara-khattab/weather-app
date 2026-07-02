97e86f85ede308b6074abaa53a8a6c05fceb6f90
yara-khattab <khatttabyara@gmail.com>
2026-07-03 02:24:00 +0300

    Restructure project folders

--- js/script.js
+++
@@ -1,5 +0,0 @@
-const btn=document.getElementById("btn")
-const info=document.getElementsByClassName("info")
-btn.addEventListener("click",()=>{
-    for(let i=0;i<info.length;i++) info[i].style.display="block"
-})
