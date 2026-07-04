14d22c9bb1db7325d72af1e30465b5db111a2167
yara-khattab <khatttabyara@gmail.com>
2026-07-04 07:04:36 +0300

    dynamicc

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,7 @@
 const btn=document.getElementById("btn")
 const info=document.getElementsByClassName("info")
+const sniper=document.getElementById("sniper")
 btn.addEventListener("click",()=>{
-    for(let i=0;i<info.length;i++) info[i].style.display="block"
+    for(let i=0;i<info.length;i++) info[i].style.display="flex"
+    sniper.style.display="block"
 })
--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
 const express=require("express")
 const app=express()
 const path=require("path")
-const x=path.join(__dirname,"../public")
-const port=process.env.port||3000
-app.use(express.static(x))
+const hbs=require("hbs")
+const port=process.env.PORT||3000
 app.listen(port, () => {
-    console.log("Server is running")
+    console.log(`Server is running on port ${port}`);
+});
+const publicPath = path.join(__dirname, "../public");
+app.use(express.static(publicPath));
+app.set('view engine','hbs')
+app.get('/',(req,res)=>{
+    res.render('index',{
+        title:"serr"
+    })
 })
+app.use("/fa", express.static("node_modules/@fortawesome/fontawesome-free"));
